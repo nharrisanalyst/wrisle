1407fc5b90695981082afadc95aa14d305164bf2
Nathan Harris <nharrisanalyst@gmail.com>
2025-02-27 14:34:24 -0800

    added header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,9 +1,14 @@
-import {render} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 
 import Header from './';
 
 describe('these are unit test for <Header/>', ()=>{
     it('renders without crashing', ()=>{
         render(<Header />)
+    })
+
+    it('displays tagline', ()=>{
+        render(<Header />);
+        expect(screen.getByText('Perception in Data'))
     })
 })
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import "./header.scss";
+
 const Header = () =>{
   return(
-    <>
-    </>
+    <div className="main-header">
+        <span id='main-tagline'>Perception in Data </span>
+    </div>
   )
 }
 
