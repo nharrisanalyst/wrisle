ae91c1bf874de3bece98b5e34a5a07524561b594
Nathan Harris <nharrisanalyst@gmail.com>
2025-03-07 18:50:03 -0800

    more work on the table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,14 @@
-import { TableState } from '../../types/TableData';
+import { useMemo } from 'react';
+import {createColumnHelper,flexRender,getCoreRowModel,useReactTable} from '@tanstack/react-table'
+import { TableState} from '../../types/TableData';
 
 export type MyTableProps ={
     tableData:TableState
 }
 
 const Table =({tableData}:MyTableProps)=>{
+    const TableType = useMemo(()=> typeof tableData.data.rows[0] ,[tableData.data.rows])
+
     return(
         <table>
         
